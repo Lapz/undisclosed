bdcad45cd372e9d0321a31a73b2a8a76b4e562d6
Travis CI User <travis@example.org>
2018-02-21 20:14:45 +0000

    ghp-upload script

--- master/underscore_util/symbol/sidebar-items.js
+++ master/underscore_util/symbol/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["FactoryMap","Maps any T to a string"],["Table",""]]});
+initSidebarItems({"struct":[["FactoryMap","Maps any T to a string"],["Table","A Scoped Map that takes any K and V"]]});
